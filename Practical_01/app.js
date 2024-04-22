904224eb2f2a4565777e58b86c0acac89700add1
Sa1ram06 <s10259930@connect.np.edu.sg>
2024-04-22 22:13:33 +0800

    Practical 1 commited

---
+++ Practical_01/app.js
@@ -0,0 +1,38 @@
+const path = require('node:path');
+
+const notes = './files/hello.txt';
+
+path.dirname(notes); // /users/joe
+path.basename(notes); // notes.txt
+path.extname(notes); // .txt
+
+//const fs = require('node:fs');
+
+/*fs.readFile('./files/notes.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+*/
+
+
+const ds = require('node:fs');
+
+ds.readFile('./files/intro.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
+
+const fs = require('node:fs');
+const content = 'hello world';
+fs.writeFile('./files/hello.txt', content, err => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
